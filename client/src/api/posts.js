da799de290e1061e286732f3da775d0c9952bed0
buddy <you@example.com>
2021-07-05 22:25:19 +0530

    Redux file structure and axios

---
+++ client/src/api/posts.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+const url = 'https://localhost:5000/posts';
+
+export const fetchPosts = () => axios.get(url); 
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Post from "./Post/Post";
 
+import useStyles from "../../styles";
+
 const Posts = () => {
+  const classes = useStyles();
+
   return (
     <>
       <h1>posts</h1>
       <Post />
       <Post />
-
-     </>
+    </>
   );
-}
+};
 
-export default Posts; 
+export default Posts;
