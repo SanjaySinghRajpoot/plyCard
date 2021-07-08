9f4e52a3b99922d6d9be08b610973624e23aaf72
buddy <you@example.com>
2021-07-08 13:13:28 +0530

    Like post method created

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -6,3 +6,4 @@ export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
 export const updatePost = (id, updatePost) => axios.patch(`${url}/${id}`, updatePost);
 export const deletePost = (id) => axios.delete(`${url}/${id}`);
+export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -4,7 +4,8 @@ export default (posts = [], action) => {  // state = posts = []
         case 'DELETE':
             return posts.filter((post) => post._id !== action.payload);
         case 'UPDATE':
-             return posts.map((post) => post._id === action.payload._id ? action.payload: post)
+        case 'LIKE':
+                return posts.map((post) => post._id === action.payload._id ? action.payload: post)
         case 'FETCH_ALL':
             return action.payload;
         case 'CREATE':
