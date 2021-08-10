929023a979b7134dad0ca081f1a64b9b8ab00a24
buddy <you@example.com>
2021-08-10 17:32:30 +0530

    delete button fixed

--- client/src/components/styles.js
+++ client/src/components/styles.js
@@ -4,6 +4,7 @@ import { makeStyles } from "@material-ui/core";
 
 export default makeStyles(() => ({
     ul: {
+        
         justifyContent: 'space-around',
         marginTop: '10%',
         marginRight: '5px'
