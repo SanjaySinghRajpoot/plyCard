d4a74b7fb95fcd390fc4d78607997da01a0ba5e0
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2021-09-05 14:31:06 +0530

    loading comment error fixed

--- client/src/components/PostDetails/CommentSection.jsx
+++ client/src/components/PostDetails/CommentSection.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useRef } from "react";
-import { Typography, TextField, Button } from "@material-ui/core";
-import { useDispatch } from "react-redux";
+import React, { useState, useRef } from 'react';
+import { Typography, TextField, Button } from '@material-ui/core/';
+import { useDispatch } from 'react-redux';
+
 
 import useStyles from "./styles";
 import { commentPost } from '../../actions/posts'; 
@@ -12,56 +13,51 @@ const CommentSection = ({ post }) => {
   const [comment, setComment] = useState("");
   const user = JSON.parse(localStorage.getItem('profile'));   // get the user from local storage
   const dispatch = useDispatch();
+  const commentsRef = useRef();
 
-  const handleClick = () => {
-      const finalComment = `${user.result.name}: ${comment}`;
+  // const handleClick = () => {
+  //     const finalComment = `${user.result.name}: ${comment}`;
 
-      dispatch(commentPost(finalComment, post._id));
+  //     dispatch(commentPost(finalComment, post._id));
+  // };
+  
+ 
+
+  const handleComment = async () => {
+    const newComments = await dispatch(commentPost(`${user?.result?.name}: ${comment}`, post._id));
+
+    setComment('');
+    setComments(newComments);
+
+    commentsRef.current.scrollIntoView({ behavior: 'smooth' });
+  
+    window.location.reload();
   };
+
 
   return (
     <div>
-      <div className={classes.commentsOuterContainer}>
-        <div className={classes.commentsInnerContainer}>
-          <Typography gutterBottom variant="h6">
-            Comments
+    <div className={classes.commentsOuterContainer}>
+      <div className={classes.commentsInnerContainer}>
+        <Typography gutterBottom variant="h6">Comments</Typography>
+        {comments?.map((c, i) => (
+          <Typography key={i} gutterBottom variant="subtitle1">
+            <strong>{c.split(': ')[0]}</strong>
+            {c.split(':')[1]}
           </Typography>
-          {comments.map((c, i) => (
-            <Typography key={i} gutterBottom variant="subtitle1">
-                Comment{i}
-            </Typography>
-          ))}
-        </div>
-
-        { user?.result?.name && ( <div style={{ width: "70%" }}>
-        <Typography gutterBottom variant="h6">
-          Type a commment
-        </Typography>
-        <TextField
-          fullWidth
-          rows={4}
-          variant="outlined"
-          label="Comment"
-          multiline
-          value={comment}
-          onChange={(e) => setComment(e.target.value)}
-        />
-        <Button
-          style={{ marginTop: "10px" }}
-          fullwidth
-          disabled={!comment}
-          variant="contained"
-          onClick={handleClick}
-          color="primary"
-        >
-         Comment
+        ))}
+        <div ref={commentsRef} />
+      </div>
+      <div style={{ width: '70%' }}>
+        <Typography gutterBottom variant="h6">Write a comment</Typography>
+        <TextField fullWidth rows={4} variant="outlined" label="Comment" multiline value={comment} onChange={(e) => setComment(e.target.value)} />
+        <br />
+        <Button style={{ marginTop: '10px' }} fullWidth disabled={!comment.length} color="primary" variant="contained" onClick={handleComment}>
+          Comment
         </Button>
       </div>
-      
-      )}
-       
-      </div>
     </div>
+  </div>
   );
 };
 
--- client/src/components/styles.js
+++ client/src/components/styles.js
@@ -1,7 +1,5 @@
 import { makeStyles } from "@material-ui/core";
 
-
-
 export default makeStyles(() => ({
     ul: {
         
