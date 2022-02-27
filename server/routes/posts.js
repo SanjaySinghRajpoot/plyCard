41d239e644f2853bc2fab1e86087615634fb438a
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2022-02-27 13:35:48 +0530

    search fixed

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,15 +1,16 @@
 import express from 'express';
 
-import { getPostsBySearch, getPosts, getPost, createPost, commentPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
+import { getPostsBySearch, getPosts, getPost, createPost, getPostsByCreator, commentPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
 
 import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.get('/:id', getPost);
+router.get('/creator', getPostsByCreator);
 router.get('/search', getPostsBySearch);
 router.get('/', getPosts);
 router.post('/', auth,createPost);
+router.get('/:id', getPost);
 router.patch('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost);
 router.patch('/:id/likePost', auth,likePost);
