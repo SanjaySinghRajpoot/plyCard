1f9e0cdd386d5af8cd5d89721c37d6809118ced6
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2022-03-04 10:28:02 +0530

    bug fixed

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -5,56 +5,93 @@ import { Link } from "react-router-dom";
 import useStyles from "./styles";
 import { useDispatch } from "react-redux";
 import { useHistory, useLocation } from "react-router-dom";
-import decode from 'jwt-decode';
+import decode from "jwt-decode";
 
 const Navbar = () => {
   const classes = useStyles();
-  const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));   
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
   const dispatch = useDispatch();
   // console.log(user);
   const history = useHistory();
   const location = useLocation();
 
   const logout = () => {
-        dispatch({ type: 'LOGOUT' });
-       
-        history.push('/');
+    dispatch({ type: "LOGOUT" });
 
-        setUser(null);
+    history.push("/");
+
+    setUser(null);
   };
 
   useEffect(() => {
     const token = user?.token;
 
-    if(token){
-       const decodedToken = decode(token);
+    if (token) {
+      const decodedToken = decode(token);
 
-       if(decodedToken.exp * 1000 < new Date().getTime()) logout();
+      if (decodedToken.exp * 1000 < new Date().getTime()) logout();
     }
 
-    setUser(JSON.parse(localStorage.getItem('profile')));  // use profile 
+    setUser(JSON.parse(localStorage.getItem("profile"))); // use profile
   }, [location]);
 
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
       <div className={classes.brandContainer}>
-        <Typography className={classes.heading} variant="h2" align="center">
-          Plycard
+        <Typography
+          className={classes.heading}
+          variant="h2"
+          align="center"
+          to="/"
+        >
+          <Link
+            to={'/'}
+            style={{ textDecoration: "none", color: "#3f51b5" }}
+          >
+            Plycard
+          </Link>
         </Typography>
         <img className={classes.image} src={plycard} alt="icon" height="60" />
       </div>
 
       <Toolbar className={classes.toolbar}>
-      {user?.result ? (
-        <div className={classes.profile}>
-          <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
-          <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
-          <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
-        </div>
-      ) : (
-        <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
-      )}
-    </Toolbar>
+        {user?.result ? (
+          <div className={classes.profile}>
+            <Avatar
+              className={classes.purple}
+              alt={user?.result.name}
+              src={user?.result.imageUrl}
+            >
+              {user?.result.name.charAt(0)}
+            </Avatar>
+            <Typography className={classes.userName} variant="h6">
+              <Link
+                to={`/creators/${user?.result.name}`}
+                style={{ textDecoration: "none", color: "#3f51b5" }}
+              >
+                {` ${user?.result.name}`}
+              </Link>
+            </Typography>
+            <Button
+              variant="contained"
+              className={classes.logout}
+              color="secondary"
+              onClick={logout}
+            >
+              Logout
+            </Button>
+          </div>
+        ) : (
+          <Button
+            component={Link}
+            to="/auth"
+            variant="contained"
+            color="primary"
+          >
+            Sign In
+          </Button>
+        )}
+      </Toolbar>
     </AppBar>
   );
 };
