fb3f226e5085b596fc637173dca2ac59d2636862
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2022-06-28 12:52:32 +0530

    Fix: routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,8 +4,8 @@ import { signin, signup } from '../controllers/user.js';
 
 const router = express.Router();
 
-router.post('./signin', signin);
-router.post('./signup', signup);
+router.post('/signin', signin);
+router.post('/signup', signup);
 
 
 
